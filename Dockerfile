703851e642360c34d16931d70c41a8163e6fed41
STEVEN COUL <stevencoul@STEVENs-Mac-Pro.local>
2019-10-15 13:40:40 -0500

    keep dockerfile

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+FROM ubuntu:16.04
+RUN apt-get -y update
+RUN adduser renogysolar
+WORKDIR /home/renogysolar
+RUN apt-get -y install bash
+RUN rm -f /bin/sh && ln -s /bin/bash /bin/sh
+RUN apt-get -y install net-tools
+RUN apt-get -y install make
+RUN apt-get -y install gcc
+RUN apt-get -y install g++
+RUN apt-get -y install libsqlite3-dev
+RUN apt-get -y install doxygen graphviz
+USER renogysolar
+WORKDIR /home/renogysolar
+
+
--- make_docker.sh
+++ make_docker.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
 PROJECT_NAME=renogysolar
-ERASE=""
-WHO=renogysolar
 
 # Kill all existing containers for the project
 EXIST_CONTAINERS=`docker ps -a --filter "ancestor=$PROJECT_NAME" -q`
@@ -12,38 +10,8 @@ docker rm $EXIST_CONTAINERS 2>/dev/null
 # Remove all existing images. 
 docker rmi $PROJECT_NAME 2>/dev/null
 
-# Create the dockerfile
-cat <<_EOF_ > Dockerfile
-FROM ubuntu:16.04
-RUN apt-get -y update
-RUN adduser $WHO
-WORKDIR /home/$WHO
-RUN apt-get -y install ssh
-RUN mkdir -p .ssh
-ADD id_rsa .ssh/id_rsa
-ADD id_rsa.pub .ssh/id_rsa.pub
-RUN chmod 600 .ssh/id_rsa
-RUN chmod 600 .ssh/id_rsa.pub
-RUN chown -R $WHO .ssh
-RUN apt-get -y install git
-RUN apt-get -y install bash
-RUN rm -f /bin/sh && ln -s /bin/bash /bin/sh
-RUN apt-get -y install vim
-RUN apt-get -y install net-tools
-RUN apt-get -y install make
-RUN apt-get -y install gcc
-RUN apt-get -y install g++
-RUN apt-get -y install libsqlite3-dev
-RUN apt-get -y install doxygen graphviz
-USER $WHO
-WORKDIR /home/$WHO
-_EOF_
-ERASE+=" Dockerfile"
-
 # Build container
 docker build -t $PROJECT_NAME .
-
-rm -f $ERASE
 
 # How to use
 # docker run -v $PWD:/home/renogysolar renogysolar make
